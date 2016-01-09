d8e0f5e2833cab740630130e14a7a89ebbbb905b
Pedro <me@pedromuller.com>
2016-01-08 23:48:38 -0600

    formmating

--- medium-follower-thanks.js
+++ medium-follower-thanks.js
@@ -1,24 +1,24 @@
-var request = require('request'),
-    mongodb = require('mongodb').MongoClient,
-    ObjectID = require('mongodb').ObjectID,
-    Twit = require('twit');
-   
-module.exports = function (ctx,done) {
-    var requestUrl = 'https://medium.com/@pedromuller/follow-list?listType=followers&page=0';
+/*jslint node: true, vars: true, nomen: true, indent: 4, maxerr: 50 */
 
-    this._mediumAccessToken = ctx.data.mediumAccessToken;
-    this._twitterAccessToken = ctx.data.twitterAccessToken;
-    this._twitterAccessToken_secret = ctx.data.twitterAccessToken_secret;
-    this._twitterConsumer_key = ctx.data.twitterConsumer_key;
-    this._twitterConsumer_secret = ctx.data.twitterConsumer_secret;
-    this._mongoUrl = ctx.data.mongoUrl;
-    
-    var makeRequest = function (URL, callback) {
-        request(
-        {
+'use strict';
+var request = require('request');
+var mongodb = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var Twit = require('twit');
+var async = require('async');
+module.exports = function (ctx, done) {
+    var requestUrl = 'https://medium.com/@pedromuller/follow-list?listType=followers&page=0';
+    this.mediumAccessToken = ctx.data.mediumAccessToken;
+    this.twitterAccessToken = ctx.data.twitterAccessToken;
+    this.twitterAccessToken_secret = ctx.data.twitterAccessToken_secret;
+    this.twitterConsumer_key = ctx.data.twitterConsumer_key;
+    this.twitterConsumer_secret = ctx.data.twitterConsumer_secret;
+    this.mongoUrl = ctx.data.mongoUrl;
+    this.makeRequest = function (URL, callback) {
+        request({
             headers: {
                 'Content-type': 'application/json',
-                'Authorization': 'Bearer ' + this._mediumAccessToken,
+                'Authorization': 'Bearer ' + this.mediumAccessToken,
                 'Accept': 'application/json',
                 'Accept-Charset': 'utf-8'
             },
@@ -31,53 +31,64 @@ module.exports = function (ctx,done) {
                 } catch (e) {
                     callback(e);
                 }
-
                 if (!!jsonResponse.payload && !!jsonResponse.payload.value) {
                     var followers = jsonResponse.payload.value.map(function (follower) {
-                        if (follower.twitterScreenName === '')
+                        if (follower.twitterScreenName === '') {
                             return;
-                        else {
-                            return follower.twitterScreenName;
                         }
+                        return follower.twitterScreenName;
                     });
-                    callback(null, followers)
+                    callback(null, followers);
                 } else {
                     callback({ 'error': 'no followers' });
                 }
-
             } else {
-                callback({ 'error': error, 'statusCode': response.statusCode, 'response': response });
+                callback({
+                    'error': error,
+                    'statusCode': response.statusCode,
+                    'response': response
+                });
             }
         });
-    }
-
-    var saveToDatabase = function (followers, callback) {
-        mongodb.connect(this._mongoUrl, function (err, db) {
+    };
+    this.saveToDatabase = function (followers, callback) {
+        var documents;
+        var difference = followers;
+        mongodb.connect(this.mongoUrl, function (err, db) {
             if (err) {
                 db.close();
                 callback(err);
             }
-            db.collection('followers').find({ twitter: { $in: followers } }, { twitter: 1, _id: 0 }).toArray(function (err, data) {
+            db.collection('followers').find({ twitter: { $in: followers } }, {
+                'twitter': 1,
+                '_id': 0
+            }).toArray(function (err, data) {
+                var i = 0;
                 if (err) {
                     callback(err);
                 }
-                var difference = followers;
                 if (data.length > 0) {
                     difference = followers.filter(function (e) {
-                        for (var i = 0; i < data.length; i++) {
+                        for (i = 0; i < data.length; i = +1) {
                             if (data[i].twitter === e) {
                                 return false;
                             }
                         }
                         return true;
                     });
-                };
-
+                }
                 if (Array.isArray(difference) && difference.length > 0) {
-                    var documents = difference.map(function (element) {
-                        return { _id: new ObjectID(), twitter: element, sent: false }
+                    documents = difference.map(function (element) {
+                        return {
+                            _id: new ObjectID(),
+                            twitter: element,
+                            sent: false
+                        };
                     });
-                    db.collection('followers').insert(documents, { continueOnError: true, safe: true }, function (err, result) {
+                    db.collection('followers').insert(documents, {
+                        continueOnError: true,
+                        safe: true
+                    }, function (err) {
                         db.close();
                         if (err) {
                             callback(err);
@@ -91,64 +102,60 @@ module.exports = function (ctx,done) {
                 return;
             });
         });
-    }
-
-    var sendTweetToFollower = function (callback) {
+    };
+    this.sendTweetToFollower = function (callback) {
         var T = new Twit({
-            consumer_key: this._twitterConsumer_key,
-            consumer_secret: this._twitterConsumer_secret,
-            access_token: this._twitterAccessToken,
-            access_token_secret: this._twitterAccessToken_secret
-        })
-
-        mongodb.connect(this._mongoUrl, function (err, db) {
+            consumer_key: this.twitterConsumer_key,
+            consumer_secret: this.twitterConsumer_secret,
+            access_token: this.twitterAccessToken,
+            access_token_secret: this.twitterAccessToken_secret
+        });
+        mongodb.connect(this.mongoUrl, function (err, db) {
             if (err) {
                 db.close();
                 callback(err);
             }
             db.collection('followers').find({ sent: false }).toArray(function (err, data) {
-                if (data.length === 0) { callback() }
-                data.forEach(function (element) {
-                    T.post('statuses/update', { status: '@' + element.twitter + ' Gracias por seguirme en Medium, un abrazo!' },
-                    function (err, data, response) {
+                if (data.length === 0) {
+                    callback();
+                } else if (err) {
+                    callback(err);
+                }
+                async.eachSeries(data, function (element, callback) {
+                    T.post('statuses/update', { status: '@' + element.twitter + ' Gracias por seguirme en Medium, un abrazo!' }, function (err) {
                         if (err) {
                             callback(err);
                         }
-                       
                         element.sent = true;
-                        db.collection('followers').updateOne({ _id: element._id }, { $set: { sent: true } }, function (err, result) {
+                        db.collection('followers').updateOne({ '_id': element._id }, { $set: { sent: true } }, function (err) {
                             db.close();
                             if (err) {
                                 callback(err);
                             }
                             callback(null);
                         });
-
                     });
+                }, function (err) {
+                    if (err) { throw err; }
                 });
             });
-
         });
-    }
-
-    makeRequest(requestUrl, function (error, followers) {
+    };
+    this.makeRequest(requestUrl, function (error, followers) {
         if (error) {
             done(error);
         } else {
-            saveToDatabase(followers, function (err) {
+            this.saveToDatabase(followers, function (err) {
                 if (err) {
                     done(error);
                 }
-                sendTweetToFollower(function (err, response) {
+                this.sendTweetToFollower(function (err) {
                     if (err) {
                         done(error);
                     }
                     done(null, 'OK');
                 });
-
             });
         }
-
     });
-    
-}
+};
